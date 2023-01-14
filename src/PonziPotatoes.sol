29966b9ab84cec5bcbb7c8793a864d6dd2ce8535
Allen Lin <allenlin1992@hotmail.com>
2023-01-13 20:56:14 -0500

    start
    Signed-off-by: Allen Lin <allenlin1992@hotmail.com>

---
+++ src/PonziPotatoes.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract PonziPotatoes is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    string public constant ponziPotatoesURL = "https://ponzipotatoes.com/api";
+    uint256 public mintCost = 0.05 ether;
+    address public previousMinter;
+
+    constructor(address _previousMinter) ERC721("Ponzi Potatoes", "PP") {
+        previousMinter = _previousMinter;
+    }
+
+    function mint() external payable {
+        require(msg.value >= mintCost * 11 / 10);
+        mintCost = mintCost * 11 / 10;
+        _safeMint(msg.sender, ponziPotatoesURL);
+        previousMinter.call{value: msg.value}("");
+        previousMinter = msg.sender;
+    }
+
+    function _safeMint(address to, string memory uri) internal {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override (ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _burn(uint256 tokenId) internal override (ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override (ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override (ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
